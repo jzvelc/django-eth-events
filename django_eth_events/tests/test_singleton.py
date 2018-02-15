d6c8b706e9ba677df71781b37136853abdf70cb1
Uxio Fuentefria <uxio@gnosis.pm>
2018-02-15 19:10:45 +0100

    Add Ethereum tester to singleton test

--- django_eth_events/tests/test_singleton.py
+++ django_eth_events/tests/test_singleton.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.test import TestCase
+from eth_tester import EthereumTester
 from web3 import RPCProvider, IPCProvider
+from web3.providers.eth_tester import EthereumTesterProvider
 from django_eth_events.web3_service import Web3Service
 from django_eth_events.event_listener import EventListener
 
 
 class TestSingleton(TestCase):
 
-    def test_single_istance(self):
+    def test_single_instance(self):
         service1 = Web3Service()
         service2 = Web3Service()
         self.assertEqual(service1.web3, service2.web3)
@@ -35,6 +37,15 @@ class TestSingleton(TestCase):
         service2 = Web3Service(ipc_provider)
         self.assertIsInstance(service2.web3.providers[0], IPCProvider)
         self.assertEqual(service2.web3.providers[0], ipc_provider)
+
+    def test_eth_tester_provider(self):
+        eth_tester_provider = EthereumTesterProvider(EthereumTester())
+
+        service1 = Web3Service()
+        self.assertIsInstance(service1.web3.providers[0], RPCProvider)
+        service2 = Web3Service(eth_tester_provider)
+        self.assertIsInstance(service2.web3.providers[0], EthereumTesterProvider)
+        self.assertEqual(service2.web3.providers[0], eth_tester_provider)
 
     def test_event_listener_singleton(self):
         ipc_provider = IPCProvider(
